bfd58ebe4a72b0a2ade6c7e9e7edb5e98bb4bb10
Vladimir902 <vladimir.malakovski@kinandcarta.com>
2024-09-04 14:19:28 +0200

    Removed unused libraries

--- src/main/java/org/example/SelectingStateAndCity.java
+++ src/main/java/org/example/SelectingStateAndCity.java
@@ -7,8 +7,7 @@ import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
-import java.time.Instant;
-import java.util.List;
+
 
 public class SelectingStateAndCity {
     public static void main(String[] args) {
